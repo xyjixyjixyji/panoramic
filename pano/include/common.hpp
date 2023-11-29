90e5f85e0ef7e18ae31fba84e53552322ed3c19d
Ji-Xinyou <jerryji0414@outlook.com>
2023-11-29 03:22:33 -0500

    feat: add timer

--- pano/include/common.hpp
+++ pano/include/common.hpp
@@ -35,4 +35,27 @@ inline void panic(const char *msg) {
   exit(1);
 }
 
+class Timer {
+public:
+  Timer(const std::string &message, const bool shouldPrint)
+      : message_(message), shouldPrint_(shouldPrint) {
+    start_ = std::chrono::high_resolution_clock::now();
+  }
+
+  ~Timer() {
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration =
+        std::chrono::duration<double, std::milli>(end - start_).count();
+    if (shouldPrint_) {
+      std::cout << message_ << ": " << std::fixed << std::setprecision(2)
+                << duration << " ms\n";
+    }
+  }
+
+private:
+  std::string message_;
+  std::chrono::high_resolution_clock::time_point start_;
+  bool shouldPrint_;
+};
+
 #endif
