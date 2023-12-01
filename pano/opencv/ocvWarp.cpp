994406e472e72c589794315d3ad661ce37ec986c
Ji-Xinyou <jerryji0414@outlook.com>
2023-12-01 02:18:57 -0500

    fix: fix opencv ransac and warping

--- pano/opencv/ocvWarp.cpp
+++ pano/opencv/ocvWarp.cpp
@@ -2,10 +2,18 @@
 #include <opencv2/imgproc.hpp>
 
 cv::Mat warpOcv(cv::Mat imageL, cv::Mat imageR, cv::Mat homography) {
-  cv::Mat warped;
-  cv::warpPerspective(imageL, warped, homography,
-                      imageL.size() + imageR.size());
-  cv::Mat half(warped, cv::Rect(0, 0, imageR.cols, imageR.rows));
-  imageR.copyTo(half);
-  return warped;
+  // Create an output image large enough to hold both images
+  cv::Mat invH = homography.inv();
+  cv::Mat warpedImage =
+      cv::Mat::zeros(std::max(imageL.rows, imageR.rows),
+                     imageL.cols + imageR.cols, imageL.type());
+
+  // Place imageL in the left part of the output image
+  imageL.copyTo(warpedImage(cv::Rect(0, 0, imageL.cols, imageL.rows)));
+
+  // Apply the modified homography transformation to imageR
+  cv::warpPerspective(imageR, warpedImage, invH, warpedImage.size(),
+                      cv::INTER_LINEAR, cv::BORDER_TRANSPARENT);
+
+  return warpedImage;
 }
