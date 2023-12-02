e4350a4ebd0d931f7d389471a2ab166c45e25e96
Zihe Zhao <zihezhao@ghc30.ghc.andrew.cmu.edu>
2023-12-02 01:38:11 -0500

    pulling

--- pano/openmp/ompHarrisMatch.cpp
+++ pano/openmp/ompHarrisMatch.cpp
@@ -3,6 +3,7 @@
 #include <matcher.hpp>
 #include <opencv2/core/types.hpp>
 #include <vector>
+#include <omp.h>
 
 OmpHarrisKeypointMatcher::OmpHarrisKeypointMatcher(cv::Mat &image1,
                                                    cv::Mat &image2,
@@ -18,6 +19,26 @@ OmpHarrisKeypointMatcher::OmpHarrisKeypointMatcher(cv::Mat &image1,
 std::vector<cv::DMatch>
 OmpHarrisKeypointMatcher::matchKeyPoints(std::vector<cv::KeyPoint> keypointsL,
                                          std::vector<cv::KeyPoint> keypointsR) {
-  return seqHarrisMatchKeyPoints(keypointsL, keypointsR, image1_, image2_,
-                                 options_, 0);
+    const int num_threads = omp_get_max_threads();
+    const int size = keypointsL.size();
+    const int chunk_size = size / num_threads;
+    std::vector<std::vector<cv::DMatch>> perThreadMatches(num_threads);
+    std::vector<cv::DMatch> allMatches;
+
+    #pragma omp parallel
+    {
+        int tid = omp_get_thread_num();
+        int start = tid * chunk_size;
+        int end = std::min(start+chunk_size, size);
+        std::vector<cv::KeyPoint> localKeypointsL(keypointsL.begin() + start,
+                                                keypointsL.begin() + end);
+        perThreadMatches[tid] = seqHarrisMatchKeyPoints(
+            localKeypointsL, keypointsR, image1_, image2_, options_, start);
+    }
+
+    for (const auto& perThreadMatch : perThreadMatches) {
+        allMatches.insert(allMatches.end(), perThreadMatch.begin(), perThreadMatch.end());
+    }
+
+    return allMatches;
 }
